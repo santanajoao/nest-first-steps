81defd3591fa1a00df04df8830a607b73777a9d3
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-12-13 22:09:59 -0300

    feat: add dto validations

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,7 +1,10 @@
 import { Task } from '@prisma/client';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 type TCreateTask = Omit<Task, 'id' | 'isDone'>;
 
 export class CreateTaskDto implements TCreateTask {
+  @IsString()
+  @IsNotEmpty()
   title: string;
 }
